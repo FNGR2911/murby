22c21ce97576bd348fcfd06bc2c5f48188d8a190
Matthias Fänger <matthias@faenger.de>
2019-10-22 17:07:39 +0200

    Bugfix frontpage look

--- web/index.php
+++ web/index.php
@@ -2,6 +2,4 @@
 
 include '../vendor/autoload.php';
 
-require 'kirby/bootstrap.php';
-
 echo (new Kirby)->render();
--- web/site/templates/default.php
+++ web/site/templates/default.php
@@ -1,9 +1,8 @@
-<?php snippet('header') ?>
-  <div id="container">
+<div id="container">
+  <?php snippet('header') ?>
     <main>
       <h1 class="title"><?= $page->title() ?></h1>
-
       <?= $page->text()->kirbytext() ?>
     </main>
-  </div>
-<?php snippet('footer') ?>
+  <?php snippet('footer') ?>
+</div>
