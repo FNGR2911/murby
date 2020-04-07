eaf01c6fb13448cf9fc66cffbf56cc7adc8938f4
Matthias Fänger <matthias@faenger.de>
2020-04-07 09:32:00 +0200

    Add ENV variable to print livereload in dev

--- web/site/snippets/footer.php
+++ web/site/snippets/footer.php
@@ -1,4 +1,6 @@
   <footer>&copy; <?php echo date("Y"); ?> Muuuurby</footer>
-  <script src="http://localhost:35729/livereload.js"></script>
+  <?php if (getenv('APP_ENV') === "dev"): ?>
+    <script src="http://localhost:35729/livereload.js"></script>
+  <?php endif;?>
 </body>
 </html>
--- web/site/templates/default.php
+++ web/site/templates/default.php
@@ -1,8 +1,8 @@
+<?php snippet('header')?>
 <div id="container">
-  <?php snippet('header') ?>
     <main>
-      <h1 class="title"><?= $page->title() ?></h1>
-      <?= $page->text()->kirbytext() ?>
+      <h1 class="title"><?=$page->title()?></h1>
+      <?=$page->text()->kirbytext()?>
     </main>
-  <?php snippet('footer') ?>
 </div>
+<?php snippet('footer')?>
